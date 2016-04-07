9c1870160f0e0b0b4225cece6d0475bc1934dd71
mvolta <marcelovolta@gmail.com>
2016-04-07 09:11:28 -0300

    https://github.com/marcelovolta/MedicalJournals.git

---
+++ MedicalJournals/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicalJournals.Startup))]
+namespace MedicalJournals
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
